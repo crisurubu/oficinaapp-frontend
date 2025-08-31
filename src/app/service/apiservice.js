ab22a180a64c24df783b581af9cd7b6d201346db
crisurubu <cristianourubu@gmail.com>
2025-08-31 15:29:27 -0300

    atualiza link da api prara o servidor render

--- src/app/service/apiservice.js
+++ src/app/service/apiservice.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const httpClient = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://oficinaapp-backend.onrender.com",
     withCredentials: true
 })
 
